6c9bdc1c02a26d3db1cb55aed5d874a8ad690cef
StewieC <makokha.stewart21@students.dkut.ac.ke>
2025-08-17 22:52:22 +0300

    continued, build a player char and manage its movements

--- PyGame/main.py
+++ PyGame/main.py
@@ -13,23 +13,46 @@ pygame.display.set_caption("space dodge")
 # setting background
 BG =pygame.transform.scale(pygame.image.load("PyGame/image.png"), (WIDTH, HEIGHT))
 
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_VEL = 5
 
-def draw():
+
+def draw(player):
     WIN.blit(BG, (0,0))
+    pygame.draw.rect(WIN, "orange", player)
     pygame.display.update()
+    
 
 # main game logic
 
 def main():
     run = True
+    
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    
+    clock = pygame.time.Clock()
+    
     # check if user clicks quit button
     while run:
+        clock.tick(60)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        # fill the window with background
-            draw()
+        
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT] and player.x - PLAYER_VEL > 0:
+                player.x -= PLAYER_VEL
+                
+            if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width < WIDTH :
+                player.x += PLAYER_VEL
+            
+            # fill the window with background
+            draw(player)
+            
+            
     pygame.quit()
     
     
